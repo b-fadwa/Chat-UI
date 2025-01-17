d435868710fdf6dc47dbf35a1ff16b2d887489ce
Fadwa BAYED <fadwa.bayed@4d.com>
2025-01-17 11:54:50 +0100

    handle message type init

--- src/components/ChatUi/ChatBody.tsx
+++ src/components/ChatUi/ChatBody.tsx
@@ -1,29 +1,53 @@
-import { MessageBox } from 'react-chat-elements';
 import { MessageList } from 'react-chat-elements';
 
 interface ChatBodyProps {
-  position: string;
   data: any;
 }
 
-// ws://192.168.225.12
+// ws://websocket
 
-const ChatBody = ({ position, data }: ChatBodyProps) => {
-  data=[data]
+const ChatBody = ({ data }: ChatBodyProps) => {
+  console.log({ data });
+  let parsedItem: any;
+  // let isSender: boolean;
 
-  console.log({ data }, { position });
+  data = data.map((item: any) => {
+    parsedItem = typeof item === 'string' && item !== '' ? JSON.parse(item) : item; //{ content: '' };
+    // isSender = parsedItem.sender.startsWith('localhost');
+    switch (typeof item) {
+      case 'string':
+        return {
+          type: 'text',
+          text: parsedItem.content,
+          title: parsedItem.sender,
+        };
+      case 'object':
+        // file object
+        if (item.content.file)
+          return {
+            type: 'file',
+            text: 'File attached:' + item.content.file.name,
+            title: 'User',
+            data: item.content.file,
+            file: item.content.file,
+            url: item.content.file,
+            // position: isSender ? 'right' : 'left', //parsedItem.sender.includes('localhost') ? 'right' : 'left',
+          };
+        //audio object
+        if (item.content.audio) {
+        }
+        //video object
+        if (item.content.video) {
+        }
+        //picture object
+        if (item.content.picture) {
+        }
+    }
+  });
 
   return (
     <div className="chat-body">
       <MessageList {...({ dataSource: data } as any)} />
-      <MessageBox
-        {...({
-          position: { position },
-          type: 'text',
-          title: 'Message Box Title',
-          text: 'Here is a text type message box',
-        } as any)}
-      />
     </div>
   );
 };
