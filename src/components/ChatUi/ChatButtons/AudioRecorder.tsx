c49cb41eb5143d0d12b86950d92cb7cc016efc4a
Imane <imane.azzaoui@4d.com>
2025-01-22 10:57:46 +0100

    AudioRecorder

---
+++ src/components/ChatUi/ChatButtons/AudioRecorder.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import { AudioRecorder } from 'react-audio-voice-recorder';
+
+interface AudioProps {
+    setAudioUri: (uri: any) => void;
+}
+
+const AudioRecorderComponent: FC<AudioProps> = ({ setAudioUri }) => {
+    // const addAudioElement = (blob: Blob) => {
+    //     const url = URL.createObjectURL(blob);
+    //     const audio = document.createElement('audio');
+    //     audio.src = url;
+    //     audio.controls = true;
+    //     document.body.appendChild(audio);
+    // };
+
+
+
+    return (
+        <div>
+            <AudioRecorder
+                onRecordingComplete={setAudioUri}
+                audioTrackConstraints={{
+                    noiseSuppression: true,
+                    echoCancellation: true,
+                    // autoGainControl,
+                    // channelCount,
+                    // deviceId,
+                    // groupId,
+                    // sampleRate,
+                    // sampleSize,
+                }}
+                onNotAllowedOrFound={(err) => console.table(err)}
+                downloadOnSavePress={true}
+                downloadFileExtension="webm"
+                mediaRecorderOptions={{
+                    audioBitsPerSecond: 128000,
+                }}
+            // showVisualizer={true}
+            />
+            <br />
+        </div>
+    );
+}
+
+export default AudioRecorderComponent;
