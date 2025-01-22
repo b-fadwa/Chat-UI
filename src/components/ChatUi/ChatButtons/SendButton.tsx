c6c9eccd3ca505883e124d77b530100e7bcf100a
Fadwa BAYED <fadwa.bayed@4d.com>
2025-01-22 10:59:01 +0100

    send button init

--- src/components/ChatUi/ChatButtons/SendButton.tsx
+++ src/components/ChatUi/ChatButtons/SendButton.tsx
@@ -4,15 +4,35 @@ import { IoIosSend } from 'react-icons/io';
 interface SendButtonProps {
   socket: WebSocket;
   message: string;
+  sentFile: File;
 }
 
-const SendButton: FC<SendButtonProps> = ({ socket, message }) => {
+const SendButton: FC<SendButtonProps> = ({ socket, message, sentFile }) => {
   const sendMessage = () => {
     if (message && socket) {
-      console.log('message sent!', JSON.stringify({ content: message }), socket);
-      socket.send(JSON.stringify({ content: message }));
+      if (sentFile) { 
+        const reader = new FileReader();  
+        reader.onload = () => {
+          const base64FileContent = reader.result?.toString();
+          const payload = {
+            content: message,
+            fileName: sentFile.name,
+            fileType: sentFile.type,
+            fileContent: base64FileContent, // Add Base64-encoded content
+          };
+  
+          socket.send(JSON.stringify({file:payload.fileContent}));
+          console.log('File sent as Base64:', payload);
+        };  
+        reader.readAsDataURL(sentFile); // Read the file as a Base64 data URL
+      } else {
+        console.log('Message sent:', message);        
+        socket.send(JSON.stringify({ content: message }));
+      }
     }
   };
+  
+  
 
   return (
     <button className="p-4 rounded bg-gray-100" onClick={sendMessage}>
