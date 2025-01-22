9c626186cf1f522b30cd08f68278e4a61c85c9ba
Fadwa BAYED <fadwa.bayed@4d.com>
2025-01-22 10:57:40 +0100

    chat body structure

--- src/components/ChatUi/ChatBody.tsx
+++ src/components/ChatUi/ChatBody.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import { MessageList } from 'react-chat-elements';
 
 interface ChatBodyProps {
@@ -6,48 +7,99 @@ interface ChatBodyProps {
 
 // ws://websocket
 
-const ChatBody = ({ data }: ChatBodyProps) => {
-  console.log({ data });
+const ChatBody: FC<ChatBodyProps> = ({ data }) => {
   let parsedItem: any;
-  // let isSender: boolean;
-
+  let isSender: boolean;
   data = data.map((item: any) => {
-    parsedItem = typeof item === 'string' && item !== '' ? JSON.parse(item) : item; //{ content: '' };
-    // isSender = parsedItem.sender.startsWith('localhost');
-    switch (typeof item) {
-      case 'string':
-        return {
-          type: 'text',
-          text: parsedItem.content,
-          title: parsedItem.sender,
-        };
-      case 'object':
-        // file object
-        if (item.content.file)
-          return {
-            type: 'file',
-            text: 'File attached:' + item.content.file.name,
-            title: 'User',
-            data: item.content.file,
-            file: item.content.file,
-            url: item.content.file,
-            // position: isSender ? 'right' : 'left', //parsedItem.sender.includes('localhost') ? 'right' : 'left',
-          };
-        //audio object
-        if (item.content.audio) {
-        }
-        //video object
-        if (item.content.video) {
-        }
-        //picture object
-        if (item.content.picture) {
-        }
+    parsedItem = JSON.parse(item);
+    isSender = parsedItem.sender ? parsedItem.sender.startsWith('localhost') : true; //to be updated
+
+    //nothing
+    if (parsedItem.content == '' && !parsedItem.file && !parsedItem.audio && !parsedItem.image) {
+      return null;
+    }
+    //text
+    if (parsedItem.content)
+      return {
+        type: 'text',
+        text: parsedItem.content,
+        title: parsedItem.sender,
+        position: isSender ? 'left' : 'right',
+      };
+    // file object
+    if (parsedItem.file)
+      return {
+        type: 'file',
+        text: 'File attached',
+        title: parsedItem.sender,
+        data: {
+          uri: parsedItem.file,
+          status: {
+            click: false,
+            loading: 0,
+          },
+          // status: {
+          //   click: true,
+          // },
+        },
+        file: parsedItem.file,
+        url: parsedItem.file,
+        position: isSender ? 'right' : 'left',
+      };
+    //audio object
+    if (parsedItem.audio) {
+      return {
+        type: 'audio',
+        title: parsedItem.sender,
+        data: {
+          audioURL: parsedItem.audio,
+          status: {
+            click: false,
+            loading: 0,
+          },
+        },
+        position: isSender ? 'right' : 'left',
+      };
+    }
+    //video object
+    if (parsedItem.video) {
+    }
+    //picture object
+    if (parsedItem.image) {
+      return {
+        type: 'photo',
+        title: parsedItem.sender,
+        data: {
+          uri: parsedItem.image,
+          status: {
+            click: false,
+            loading: 0,
+          },
+        },
+        position: isSender ? 'right' : 'left',
+      };
     }
   });
 
+  const handleDownload = (message: any) => {
+    const fileUrl = message.data?.uri || message.data?.audioURL || message.data.url; //image or audio or file
+    if (fileUrl) {
+      const link = document.createElement('a');
+      link.href = fileUrl;
+      link.download = message.type;
+      link.click();
+    }
+  };
+
   return (
     <div className="chat-body">
-      <MessageList {...({ dataSource: data } as any)} />
+      <MessageList
+        {...({ dataSource: data } as any)}
+        onDownload={(message: any) => {
+          handleDownload(message);
+        }}
+        className="message-list bg-red-200"
+      />
     </div>
   );
 };
