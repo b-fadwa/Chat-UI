da473a1a156d5bd04935913fee9df130e7a072e9
Fadwa BAYED <fadwa.bayed@4d.com>
2025-02-27 16:01:49 +0000

    add go back to convo button

--- src/components/ChatUi/ChatButtons/Users.tsx
+++ src/components/ChatUi/ChatButtons/Users.tsx
@@ -4,9 +4,16 @@ import { ChatList } from 'react-chat-elements';
 interface UsersBarProps {
   allUsers: any[];
   setSelectedUser: (user: any) => void;
+  setShowUsers: (show: boolean) => void;
+  setSelectedConversation: (conversation: any) => void;
 }
 
-const UsersBar: FC<UsersBarProps> = ({ allUsers, setSelectedUser }) => {
+const UsersBar: FC<UsersBarProps> = ({
+  allUsers,
+  setSelectedUser,
+  setShowUsers,
+  setSelectedConversation,
+}) => {
   const data = allUsers.map((user) => {
     return {
       avatar: user.avatar || 'https://img.freepik.com/free-icon/user_318-804790.jpg', //to fix
@@ -16,7 +23,7 @@ const UsersBar: FC<UsersBarProps> = ({ allUsers, setSelectedUser }) => {
   });
 
   return (
-    <div className=" overflow-auto">
+    <div className="flex grow flex-col h-full justify-between">
       {data.length === 0 ? (
         <p className="text-gray-500 text-center">All Users</p>
       ) : (
@@ -27,9 +34,18 @@ const UsersBar: FC<UsersBarProps> = ({ allUsers, setSelectedUser }) => {
               setSelectedUser(user.title);
             },
           } as any)}
-          className="chat-bar-list"
+          className="chat-bar-list overflow-auto"
         />
       )}
+      <button
+        className="go-to-users p-4 bg-gray-100 bottom-0 sticky"
+        onClick={() => {
+          setSelectedConversation(null);
+          setShowUsers(false);
+        }}
+      >
+        Go back to conversations
+      </button>
     </div>
   );
 };
